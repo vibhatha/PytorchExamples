b675943cfed40270e175ae4bc0e78b67515b735d
vibhatha <vibhatha@gmail.com>
2020-02-17 15:13:29 -0500

    adding model parallel versions

--- parallel/model/alexnet_model_parallel_v1.py
+++ parallel/model/alexnet_model_parallel_v1.py
@@ -76,10 +76,10 @@ class AlexNet(nn.Module):
         return x
 
 
-class ModelParallelAlexNet(AlexNet):
+class ModelParallelAlexNetV1(AlexNet):
 
     def __init__(self, num_classes=1000, devices=['cuda:0', 'cuda:1']):
-        super(ModelParallelAlexNet, self).__init__(num_classes=num_classes)
+        super(ModelParallelAlexNetV1, self).__init__(num_classes=num_classes)
         self.seq1 = nn.Sequential(
             self.conv1,
             self.relu1,
@@ -215,6 +215,251 @@ class ModelParallelAlexNetV3(AlexNet):
         return self.fc3(x)
 
 
+class ModelParallelAlexNetV4(AlexNet):
+
+    def __init__(self, num_classes=1000, devices=['cuda:0', 'cuda:1',
+                                                  'cuda:2', 'cuda:3',
+                                                  'cuda:4']):
+        super(ModelParallelAlexNetV4, self).__init__(num_classes=num_classes)
+        self.seq1 = nn.Sequential(
+            self.conv1,
+            self.relu1,
+            self.maxpool1,
+        ).to(devices[0])
+
+        self.seq2 = nn.Sequential(
+            self.conv2,
+            self.relu2,
+            self.maxpool2,
+        ).to(devices[1])
+
+        self.seq3 = nn.Sequential(
+            self.conv3,
+            self.relu3,
+            self.conv4,
+            self.relu4,
+        ).to(devices[2])
+
+        self.seq4 = nn.Sequential(
+            self.conv5,
+            self.relu5,
+            self.maxpool3,
+            self.avgpool
+        ).to(devices[3])
+
+        #
+        # self.pool = nn.Sequential(
+        #     self.avgpool
+        # ).to(devices_layer_mapping[0])
+
+        self.seq5 = nn.Sequential(
+            self.dropout1,
+            self.fc1,
+            self.fc1_relu,
+            self.dropout2,
+            self.fc2,
+            self.fc2_relu,
+        ).to(devices[4])
+
+        self.fc3.to(devices[4])
+
+    def forward(self, x):
+        x = self.seq1(x).to('cuda:1')
+        x = self.seq2(x).to('cuda:2')
+        x = self.seq3(x).to('cuda:3')
+        x = self.seq5(torch.flatten(self.seq4(x), 1).to('cuda:4'))
+        return self.fc3(x)
+
+
+class ModelParallelAlexNetV5(AlexNet):
+
+    def __init__(self, num_classes=1000, devices=['cuda:0', 'cuda:1',
+                                                  'cuda:2', 'cuda:3',
+                                                  'cuda:4', 'cuda:5']):
+        super(ModelParallelAlexNetV5, self).__init__(num_classes=num_classes)
+        self.seq1 = nn.Sequential(
+            self.conv1,
+            self.relu1,
+            self.maxpool1,
+        ).to(devices[0])
+
+        self.seq2 = nn.Sequential(
+            self.conv2,
+            self.relu2,
+            self.maxpool2,
+        ).to(devices[1])
+
+        self.seq3 = nn.Sequential(
+            self.conv3,
+            self.relu3,
+        ).to(devices[2])
+
+        self.seq4 = nn.Sequential(
+            self.conv4,
+            self.relu4,
+        ).to(devices[3])
+
+        self.seq5 = nn.Sequential(
+            self.conv5,
+            self.relu5,
+            self.maxpool3,
+            self.avgpool
+        ).to(devices[4])
+
+        #
+        # self.pool = nn.Sequential(
+        #     self.avgpool
+        # ).to(devices_layer_mapping[0])
+
+        self.seq6 = nn.Sequential(
+            self.dropout1,
+            self.fc1,
+            self.fc1_relu,
+            self.dropout2,
+            self.fc2,
+            self.fc2_relu,
+        ).to(devices[5])
+
+        self.fc3.to(devices[5])
+
+    def forward(self, x):
+        x = self.seq1(x).to('cuda:1')
+        x = self.seq2(x).to('cuda:2')
+        x = self.seq3(x).to('cuda:3')
+        x = self.seq4(x).to('cuda:4')
+        x = self.seq6(torch.flatten(self.seq5(x), 1).to('cuda:5'))
+        return self.fc3(x)
+
+
+class ModelParallelAlexNetV6(AlexNet):
+
+    def __init__(self, num_classes=1000, devices=['cuda:0', 'cuda:1',
+                                                  'cuda:2', 'cuda:3',
+                                                  'cuda:4', 'cuda:5',
+                                                  'cuda:6']):
+        super(ModelParallelAlexNetV6, self).__init__(num_classes=num_classes)
+        self.seq1 = nn.Sequential(
+            self.conv1,
+            self.relu1,
+            self.maxpool1,
+        ).to(devices[0])
+
+        self.seq2 = nn.Sequential(
+            self.conv2,
+            self.relu2,
+            self.maxpool2,
+        ).to(devices[1])
+
+        self.seq3 = nn.Sequential(
+            self.conv3,
+            self.relu3,
+        ).to(devices[2])
+
+        self.seq4 = nn.Sequential(
+            self.conv4,
+            self.relu4,
+        ).to(devices[3])
+
+        self.seq5 = nn.Sequential(
+            self.conv5,
+            self.relu5,
+            self.maxpool3,
+            self.avgpool
+        ).to(devices[4])
+
+        #
+        # self.pool = nn.Sequential(
+        #     self.avgpool
+        # ).to(devices_layer_mapping[0])
+
+        self.seq6 = nn.Sequential(
+            self.dropout1,
+            self.fc1,
+            self.fc1_relu,
+        ).to(devices[5])
+
+        self.seq7 = nn.Sequential(
+            self.dropout2,
+            self.fc2,
+            self.fc2_relu,
+        ).to(devices[6])
+
+        self.fc3.to(devices[6])
+
+    def forward(self, x):
+        x = self.seq1(x).to('cuda:1')
+        x = self.seq2(x).to('cuda:2')
+        x = self.seq3(x).to('cuda:3')
+        x = self.seq4(x).to('cuda:4')
+        x = self.seq6(torch.flatten(self.seq5(x), 1).to('cuda:5')).to('cuda:6')
+        x = self.seq7(x)
+        return self.fc3(x)
+
+
+class ModelParallelAlexNetV7(AlexNet):
+
+    def __init__(self, num_classes=1000, devices=['cuda:0', 'cuda:1',
+                                                  'cuda:2', 'cuda:3',
+                                                  'cuda:4', 'cuda:5',
+                                                  'cuda:6', 'cuda:7']):
+        super(ModelParallelAlexNetV7, self).__init__(num_classes=num_classes)
+        self.seq1 = nn.Sequential(
+            self.conv1,
+            self.relu1,
+            self.maxpool1,
+        ).to(devices[0])
+
+        self.seq2 = nn.Sequential(
+            self.conv2,
+            self.relu2,
+            self.maxpool2,
+        ).to(devices[1])
+
+        self.seq3 = nn.Sequential(
+            self.conv3,
+            self.relu3,
+        ).to(devices[2])
+
+        self.seq4 = nn.Sequential(
+            self.conv4,
+            self.relu4,
+        ).to(devices[3])
+
+        self.seq5 = nn.Sequential(
+            self.conv5,
+            self.relu5,
+            self.maxpool3,
+            self.avgpool
+        ).to(devices[4])
+
+        #
+        # self.pool = nn.Sequential(
+        #     self.avgpool
+        # ).to(devices_layer_mapping[0])
+
+        self.seq6 = nn.Sequential(
+            self.dropout1,
+            self.fc1,
+            self.fc1_relu,
+        ).to(devices[5])
+
+        self.seq7 = nn.Sequential(
+            self.dropout2,
+            self.fc2,
+            self.fc2_relu,
+        ).to(devices[6])
+
+        self.fc3.to(devices[7])
+
+    def forward(self, x):
+        x = self.seq1(x).to('cuda:1')
+        x = self.seq2(x).to('cuda:2')
+        x = self.seq3(x).to('cuda:3')
+        x = self.seq4(x).to('cuda:4')
+        x = self.seq6(torch.flatten(self.seq5(x), 1).to('cuda:5')).to('cuda:6')
+        x = self.seq7(x).to('cuda:7')
+        return self.fc3(x)
+
 
 num_classes = 1000
 num_batches = 1
@@ -249,7 +494,7 @@ def train(model):
         optimizer.zero_grad()
         outputs = model(inputs.to('cuda:0'))
 
-        #print("Output-device {}".format(outputs.device))
+        # print("Output-device {}".format(outputs.device))
 
         # run backward pass
         labels = labels.to(outputs.device)
@@ -257,39 +502,16 @@ def train(model):
         optimizer.step()
 
 
-
-
 stmt = "train(model)"
 
-setup = "model = ModelParallelAlexNet(num_classes=num_classes)"
+for version in range(7):
+    setup = "model = ModelParallelAlexNetV{}(num_classes=num_classes)".format(
+        version)
 
-rn_run_times = timeit.repeat(
-            stmt, setup, number=1, repeat=num_repeat, globals=globals())
-rn_mean, rn_std = np.mean(rn_run_times), np.std(rn_run_times)
+    rn_run_times = timeit.repeat(
+        stmt, setup, number=1, repeat=num_repeat, globals=globals())
+    rn_mean, rn_std = np.mean(rn_run_times), np.std(rn_run_times)
 
-print("Model Parallel Training Time:", rn_mean)
-
-###############################################################################
-
-stmt = "train(model)"
-
-setup = "model = ModelParallelAlexNetV2(num_classes=num_classes)"
-
-rn_run_times = timeit.repeat(
-            stmt, setup, number=1, repeat=num_repeat, globals=globals())
-rn_mean, rn_std = np.mean(rn_run_times), np.std(rn_run_times)
-
-print("Model Parallel V2 Training Time:", rn_mean)
-
-###############################################################################
-
-stmt = "train(model)"
-
-setup = "model = ModelParallelAlexNetV3(num_classes=num_classes)"
-
-rn_run_times = timeit.repeat(
-            stmt, setup, number=1, repeat=num_repeat, globals=globals())
-rn_mean, rn_std = np.mean(rn_run_times), np.std(rn_run_times)
-
-print("Model Parallel V3 Training Time:", rn_mean)
-
+    print("Devices {}: Model Parallel V{} Training Time: {}", version,
+          str(version + 1),
+          rn_mean)
